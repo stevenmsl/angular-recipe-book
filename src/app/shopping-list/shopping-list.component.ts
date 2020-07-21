727603c03b06f2d5e2ef102c077228b6b1d0430f
Steven Lin <steven_lx@outlook.com>
2020-07-20 17:14:14 -0700

    add shopping list state

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { LoggingService } from '../logging.service';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-shopping-list',
@@ -10,7 +11,7 @@ import { LoggingService } from '../logging.service';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
   private igChangeSub: Subscription;
   // ingredients: Ingredient[] = [
   //   new Ingredient('Apples', 5),
@@ -18,16 +19,27 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   // ];
   constructor(
     private slService: ShoppingListService,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    /* 
+      - area: shoppingList
+      - you can find what the area should be in in the 
+        app.module:
+        StoreModule.forRoot({
+        shoppingList: shoppingListReducer,
+        }),
+      - the type of the area (shoppingList) should be the same
+        as the return type of the initial state defined in the 
+        reducer:
+        const initialState = {
+          ingredients: [new Ingredient('Apples', 5), 
+          new Ingredient('Tomatoes', 10)],
+        };
+    */
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
   ) {}
 
   ngOnInit(): void {
-    this.ingredients = this.slService.getIngredients();
-    this.igChangeSub = this.slService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
+    this.ingredients = this.store.select('shoppingList');
 
     this.loggingService.printLog('Hello from ShoppingListComponent');
   }
@@ -38,9 +50,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+    //this.ingredients.push(ingredient);
   }
-  ngOnDestroy() {
-    this.igChangeSub.unsubscribe();
-  }
+  ngOnDestroy() {}
 }
---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,23 @@
+import { Ingredient } from '../../shared/ingredient.model';
+import * as ShoppingListActions from './shopping-list.actions';
+
+const initialState = {
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
+};
+
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.AddIngredient
+) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state, // copy everytning
+        // overwrite what you have modified
+        ingredients: [...state.ingredients, action.type],
+      };
+    // crucial to initialize the state
+    default:
+      return state;
+  }
+}
